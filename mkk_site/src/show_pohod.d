4fe07c30294848a7c5ae0a3e1a104e5839fb5193
Prokop <prokop.yar@gmail.com>
2013-05-14 14:46:36 +0400

    Внёс изменения в Поход

--- mkk_site/src/show_pohod.d
+++ mkk_site/src/show_pohod.d
@@ -21,9 +21,9 @@ int main()
 	with(FieldType) {
 	touristRecFormat = RecordFormat(
 	[IntKey, Str, Str, Str, Str, Str, Str, Str, Str],
-	["Ключ", "Номер", "Сроки <br> похода", "Вид, кс","Район","Руководитель","Город,<br>организация", "Комментарий","Статус<br> похода"],
+	["Ключ", "Номер", "Сроки <br> похода", "Вид, кс","Район","Руководитель","Участники","Город,<br>организация", "Комментарий","Статус<br> похода"],
 	//[null, null, null, null, null, null,null, null, null],
-	[true, true, true, true, true, true, true, true, true] //Разрешение нулевого значения
+	[true, true, true, true, true, true, true, true,true, true] //Разрешение нулевого значения
 	);
 	}
 	
@@ -34,18 +34,41 @@ int main()
 	
 	//SELECT num, femelu, neim, otchectv0, brith_date, god, adrec, telefon, tel_viz FROM turistbl;
 	string queryStr = 
+	`with 
+tourist_nums as (
+  select num, unnest(unit_neim) as tourist_num from pohod
+),  `
+
+   `  tourist_info as (
+select tourist_nums.num, tourist_num, family_name, given_name, patronymic, birth_year from tourist_nums
+  join tourist 
+   on tourist_num = tourist.num
+), `
+
+`U as (
+select num, string_agg(
+  family_name||' '||given_name||' '||patronymic||', '||birth_year::text, '<br>') as gr
+from tourist_info
+group by num
+) `
+
 	  `select pohod.num, (coalesce(kod_mkk,'')||'<br>'||coalesce(nomer_knigi,'')) as nomer_knigi, `  
      `(coalesce(begin_date::text,'')||'<br>'||coalesce(finish_date::text,'')) as date , ` 
      `( coalesce( vid::text, '' )||'<br>'|| coalesce( ks::text, '' )||coalesce( element::text, '' ) ) as vid,`
 
      `region_pohod ,`
-     `(tourist.family_name||'<br>'||coalesce(tourist.given_name,'')||'<br>'||coalesce(patronymic,'')),`
-     `(coalesce(organization,'')||'<br>'||coalesce(region_group,'')),`
+     `(tourist.family_name||'<br>'||coalesce(tourist.given_name,'')||'<br>'||coalesce(tourist.patronymic,'')||'<br>'||coalesce(tourist.birth_year::text,'')), `
+     `(' title="'||coalesce(gr,'')||'"'||pohod.unit), `
+     
+     `(coalesce(organization,'')||'<br>'||coalesce(region_group,'')), `
      `(coalesce(marchrut,'')||'<br>'||coalesce(chef_coment,'')),`
      `(coalesce(prepare::text,'')||'<br>'||coalesce(status::text,''))  `
-             `from pohod `
-                   `JOIN tourist `
-                          `on pohod.chef_grupp = tourist.num`
+             `from pohod  `
+               
+        ` JOIN tourist   `
+      `on pohod.chef_grupp = tourist.num `
+      `join U `
+      `on U.num = pohod.num `        
 
 ;
 		
@@ -54,7 +77,7 @@ int main()
 	
 	auto rsView = new RecordSetView(rs);
 	with( FieldOutputMode )
-		rsView._outputModes = [visible, visible, visible, visible, visible, visible, visible, visible, visible];
+		rsView._outputModes = [visible, visible, visible, visible,visible, visible, visible, visible, visible, visible];
 	rsView._viewManners = [FieldViewManner.plainText, FieldViewManner.plainText/*, FieldViewManner.simpleControls*/];
 	rsView._HTMLClasses = [`cod`, `book-name`, `author`];
 	string html = `<html><body><head><link rel="stylesheet" type="text/css" href="` ~ projectPath ~ `/css/full_test.css">` 
