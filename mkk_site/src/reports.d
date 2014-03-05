3cecc4d4b5167adb5c56ab06bc6bd4ea47f0f96c
test_serv <test_serv@Andromeda>
2014-03-05 14:05:23 +0400

    Добавлены ссылки на походы 2043 года

--- mkk_site/src/reports.d
+++ mkk_site/src/reports.d
@@ -31,14 +31,19 @@ void netMain(HTTPContext context)
 	string содержимоеГлавнойСтраницы = `
 	<h2>Отчёты о походах</h2>
 	
+	<p>Отчёты 2013 года.  </p>
+	<p><a href="http://yadi.sk/d/9P4YLRL3JqsY8"         >Жомболок - Ока.     </a></p>
+	<p><a href="http://yadi.sk/d/J6qlFdMoJqsZZ"         >Чирка - Кемь.   </a></p>
+	<p><a href="http://yadi.sk/d/a42c7qJY943eD"         >Косью.   </a></p>
+	
 	<p>Отчёты 2012 года.  </p>
-	<p><a href="/pub/reports/2012/Отчет кавказ 2012.doc"           >Отчет Кавказ.     </a></p>
-	<p><a href="/pub/reports/2012/Отчет Камчатка 2012.doc"         >Отчет Камчатка.   </a></p>
-	<p><a href="/pub/reports/2012/Отчет Она Абакан 2012.doc"       >Отчет Она Абакан. </a></p>
-	<p><a href="/pub/reports/2012/Отчет Ю.Буг 2012.doc"            >Отчет Ю.Буг.      </a></p>
-	<p><a href="/pub/reports/2012/Отчёт Лахость 2012.doc"           >Отчёт Лахость.     </a></p>
-	<p><a href="/pub/reports/2012/Отчета  Писта 2012.doc"           >Отчета  Писта.     </a></p>
-	<p><a href="/pub/reports/2012/Сводный 2012.xlsx"           >Сводный чемпионата походов <br> Ярославской области 2012 года.xlsx.     </a></p>
+	<p><a href="/pub/reports/2012/Отчет кавказ 2012.doc"           > Кавказ.     </a></p>
+	<p><a href="/pub/reports/2012/Отчет Камчатка 2012.doc"         > Камчатка.   </a></p>
+	<p><a href="/pub/reports/2012/Отчет Она Абакан 2012.doc"       > Она Абакан. </a></p>
+	<p><a href="/pub/reports/2012/Отчет Ю.Буг 2012.doc"            > Ю.Буг.      </a></p>
+	<p><a href="/pub/reports/2012/Отчёт Лахость 2012.doc"           > Лахость.     </a></p>
+	<p><a href="/pub/reports/2012/Отчета  Писта 2012.doc"           >  Писта.     </a></p>
+	<p><a href="/pub/reports/2012/Сводный.xlsx"           >Сводный чемпионата походов Ярославской области 2012 года.xlsx.     </a></p>
 	<p><a href="/pub/reports/2012/Marshrutka.doc"           >Маршрутная книжка.     </a></p>
 	
 	
