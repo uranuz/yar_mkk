ca49e3fbf7af9229f207bb115b0e4e38104eb823
uranuz <uranuz@freelancer>
2014-06-14 14:19:12 +0400

    Частично задокументирован модуль db.database

--- webtank/src/db/database.d
+++ webtank/src/db/database.d
@@ -1,38 +1,171 @@
 module webtank.db.database;
-///Какие-то там базовые интерфейсы по работе с базой данных
-//Большой пользы пока не приносят, но как бы украшают...
 
 import std.conv;
 
-///Тип СУБД, или по-буржуйски Database Management System (DBMS)
+/++
+$(LOCALE_EN_US	Enumerated type representing types of database
+	management system (DBMS)
+)
+$(LOCALE_RU_RU Перечислимый тип, представляющий типы систем
+	управления базами данных (СУБД)
+)
++/
 enum DBMSType {PostgreSQL, MySQL, Firebird}; //Вроде как на будущее
 
+/++
+$(LOCALE_EN_US Base interface for database)
+$(LOCALE_RU_RU Базовый интерфейс для базы данных)
++/
 interface IDatabase ///Интерфейс БД
-{	bool connect(string connStr);  //Подключение к БД, используя строку подключения
-	bool isConnected() @property;  //Вернёт true, если мы подключены к БД
-	IDBQueryResult query(const(char)[] queryStr);  //Подача запроса к БД, возвращает результат
+{
+	/++
+	$(LOCALE_EN_US
+		Connects to database using connection string. Returns true if
+		connection is succesful or false if not
+	)
+	$(LOCALE_RU_RU
+		Подключается к базе данных, используя строку подключения.
+		Возвращает true при успешном подключении или false в противном
+		случае
+	)
+	+/
+	bool connect(string connStr);
+
+	/++
+	$(LOCALE_EN_US Property returns true if connection establised)
+	$(LOCALE_RU_RU Свойство возвращает true при установленном подключении)
+	+/
+	bool isConnected() @property;
+
+	/++
+	$(LOCALE_EN_US
+		Method executes query to database represented by $(D_PARAM queryStr).
+		Returns database query result $(D IDBQueryResult)
+	)
+	$(LOCALE_RU_RU
+		Метод выполняет запрос к базе данных представленный строкой запроса
+		$(D_PARAM queryStr). Возвращает результат запроса $(D IDBQueryResult)
+	)
+	+/
+	IDBQueryResult query(const(char)[] queryStr);
 	//DBStatus getStatus() @property; //Подробнее узнать как дела у базы
+
+	/++
+	$(LOCALE_EN_US
+		Property returns last error message when executing query. Returns
+		null if there is no error
+	)
+	$(LOCALE_RU_RU
+		Свойство возвращает строку с сообщением о последней ошибке.
+		Возвращает пустое значение (null), если ошибок нет
+	)
+	+/
 	string lastErrorMessage() @property; //Прочитать последнюю ошибку
 	//string getVersionInfo();
-	DBMSType type() @property; //Тип системы управления БД
-	
-	void disconnect(); //Отключение от БД
+
+	/++
+	$(LOCALE_EN_US Property returns type of database)
+	$(LOCALE_RU_RU Свойство возвращает тип базы данных)
+	+/
+	DBMSType type() @property;
+
+	/++
+	$(LOCALE_EN_US Method disconnects from database server)
+	$(LOCALE_RU_RU Метод отключения от сервера баз данных)
+	+/
+	void disconnect();
 }
 
-interface IDBQueryResult ///Интерфейс результата запроса к БД
+/++
+$(LOCALE_EN_US Base interface for data base query result)
+$(LOCALE_RU_RU Базовый интерфейс для результата запроса к базе данных)
++/
+interface IDBQueryResult
 {	/+DBMSType type() @property; //Снова тип СУБД+/
-	size_t recordCount() @property; //Число записей
-	size_t fieldCount() @property;  //Число полей данных
+	/++
+	$(LOCALE_EN_US Property returns record count in result)
+	$(LOCALE_RU_RU Свойство возвращает количество записей в результате)
+	+/
+	size_t recordCount() @property;
+
+	/++
+	$(LOCALE_EN_US Property returns field count in result)
+	$(LOCALE_RU_RU Свойство возвращает количество полей в результате)
+	+/
+	size_t fieldCount() @property;
+
+	/++
+	$(LOCALE_EN_US Method clears object and frees resources of result)
+	$(LOCALE_RU_RU Метод очищает объект и освобождает ресурсы результата)
+	+/
 	void clear(); //Очистить объект
-	
+
+	/++
+	$(LOCALE_EN_US
+		Method returns name of field in result by field index $(D_PARAM index)
+	)
+	$(LOCALE_RU_RU
+		Метод возвращает имя поля в результате по индексу поля $(D_PARAM index)
+	)
+	+/
 	string getFieldName(size_t index);
+
+	/++
+	$(LOCALE_EN_US
+		Method returns index of field in result by field name $(D_PARAM name)
+	)
+	$(LOCALE_RU_RU
+		Метод возвращает номер поля в результате по имени поля $(D_PARAM name)
+	)
+	+/
 	size_t getFieldIndex(string name);
+
+	/++
+	$(LOCALE_EN_US
+		Method returns true if cell with field index $(D_PARAM fieldIndex) and
+		record index $(D_PARAM recordIndex) is null or false otherwise
+	)
+	$(LOCALE_RU_RU
+		Метод возвращает true, если ячейка с номером поля $(D_PARAM fieldIndex)
+		и номером записи $(D_PARAM recordIndex) является пустой (null) или
+		false в противном случае
+	)
+	+/
 	bool isNull(size_t fieldIndex, size_t recordIndex);
+
+	/++
+	$(LOCALE_EN_US
+		Method returns value of celll with field index $(D_PARAM fieldIndex) and
+		record index $(D_PARAM recordIndex). If cell is null then behaviour is
+		undefined
+	)
+	$(LOCALE_RU_RU
+		Метод возвращает значение ячейки с номером поля $(D_PARAM fieldIndex)
+		и номером записи $(D_PARAM recordIndex). Если ячейка пуста (null), то
+		поведение не определено
+	)
+	+/
 	string get(size_t fieldIndex, size_t recordIndex);
+
+	/++
+	$(LOCALE_EN_US
+		Method returns value of celll with field index $(D_PARAM fieldIndex) and
+		record index $(D_PARAM recordIndex). $(D_PARAM defaultValue) parameter
+		sets return value when cell is null
+	)
+	$(LOCALE_RU_RU
+		Метод возвращает значение ячейки с номером поля $(D_PARAM fieldIndex)
+		и номером записи $(D_PARAM recordIndex). Параметр метода
+		$(D_PARAM defaultValue) задает возвращаемое значение, если ячейка пуста (null)
+	)
+	+/
 	string get(size_t fieldIndex, size_t recordIndex, string defaultValue);
 }
 
-///Класс исключений при работе с БД
+/++
+$(LOCALE_EN_US Exception class for database)
+$(LOCALE_RU_RU Класс исключений при работе с БД)
++/
 class DBException : Exception {
 	this(string msg, string file = __FILE__, size_t line = __LINE__) {
 		super(msg, file, line);
