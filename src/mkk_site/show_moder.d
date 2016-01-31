d2be873b60a0d0ae76e1532b7b1b2a65cb56c118
Uranuz <neuranuz@gmail.com>
2016-01-31 21:05:17 +0300

    Переработал код страниц show_moder и index для соответствия шаблону MVC

--- src/mkk_site/show_moder.d
+++ src/mkk_site/show_moder.d
@@ -7,7 +7,7 @@ import webtank.datctrl.data_field, webtank.datctrl.record_format, webtank.db.pos
 
 import mkk_site;
 
-immutable(string) thisPagePath;
+static immutable string thisPagePath;
 
 shared static this()
 {	
@@ -15,51 +15,80 @@ shared static this()
 	PageRouter.join!(netMain)(thisPagePath);
 }
 
-string netMain(HTTPContext context)
-{	
-	auto rq = context.request;
-
-	string content;
-
-	auto authDB = getAuthDB(); //Объект подключения к БД аутентификации
-	
+class Moder
+{
+public:
 	///Начинаем оформлять таблицу с данными
-	static immutable touristRecFormat = RecordFormat!(
+	static immutable moderRecFormat = RecordFormat!(
 		PrimaryKey!(size_t), "Ключ",
 		string, "ФИО", 
 		string, "Статус", 
-		string, "Контакты"
+		string, "Контакты",
+		size_t, "Ключ туриста"
 	)();
 	
-	string queryStr = 
+	static immutable moderListQuery = 
 `select 
-	num, name, 
-	( coalesce(status,'') || coalesce(', ' || region, '') ) as stat,
-	( coalesce(email,'') || coalesce('<br>' || contact_info, '') ) as contact 
-from site_user order by num 
-;`; 
+	num as "Ключ", 
+	name as "ФИО", 
+	( coalesce(status,'') || coalesce(', ' || region, '') ) as "Статус",
+	( coalesce(email,'') || coalesce('<br>' || contact_info, '') ) as "Контакты",
+	tourist_num as "Ключ туриста"
 	
-	auto rs = authDB.query(queryStr).getRecordSet(touristRecFormat);
+	from site_user
+	where "user_group" in ('moder', 'admin')
+	order by num
+;`;
 	
-	string table = `<div><table class="tab1">`;
-		
-	table ~=`<tr><th>Имя</th><th>Статус</th><th> Контакты</th></tr>`;
-	foreach(rec; rs)
-	{	table ~= `<tr>`;
-		
-		table ~= `<td>` ~ rec.get!"ФИО"("") ~ `</td>`;
-		table ~= `<td>` ~ rec.get!"Статус"("") ~ `</td>`;
-	      //table ~= `<td>` ~rec.get!"Опыт"("нет")  ~ `</td>`;
-		table ~= `<td>` ~ rec.get!"Контакты"("нет") ~ `</td>`;		
-	      //table ~= `<td>` ~ rec.get!"Комментарий"("нет") ~ `</td>`;
-				
-		table ~= `</tr>`;
+	static auto list()
+	{
+		return getAuthDB()
+			.query(moderListQuery)
+			.getRecordSet(moderRecFormat);
 	}
-	table ~= `</table></div>`;
+}
 
-	content ~= table; //Тобавляем таблицу с данными к содержимому страницы
-	
-	return content;
+class ModerView
+{
+public:
+	static string render(RS)(RS rs)
+	{
+		import std.array: appender;
+		
+		auto table = appender!string();
+		
+		table ~= `<div><table class="tab1">`;
+		
+		table ~=`<tr><th>Имя</th><th>Статус</th><th>Контакты</th></tr>`;
+		foreach(rec; rs)
+		{	
+			table ~= `<tr>`;
+			
+			table ~= `<td>`;
+				if( rec.isNull("Ключ туриста") )
+					table ~= rec.get!"ФИО"("");
+				else //Делаем ссылку на туриста, если пользователь связан с туристом
+					table ~= `<a href="` ~ dynamicPath ~ "show_pohod_for_tourist?key=" ~ rec.getStr!"Ключ туриста"() ~ `">` ~ rec.get!"ФИО"("") ~ `</a>`;
+					
+			table ~= `</td>`;
+			
+			table ~= `<td>` ~ rec.get!"Статус"("") ~ `</td>`
+				~ `<td>` ~ rec.get!"Контакты"("нет") ~ `</td>`;
+			
+			table ~= `</tr>`;
+		}
+		table ~= `</table></div>`;
+
+		return table.data();
+	}
+
+}
+
+string netMain(HTTPContext context)
+{	
+	auto rs = Moder.list();
+
+	return ModerView.render(rs);
 }
 
 
