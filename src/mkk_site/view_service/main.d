e29fdeb6dcd91471a9c35de036012540ef9d8248
oleg <devnull@localhost>
2017-04-23 09:33:03 +0300

    Перевод страницы TURISTS на новый шаблонизатор

--- src/mkk_site/view_service/main.d
+++ src/mkk_site/view_service/main.d
@@ -4,11 +4,15 @@ import webtank.net.web_server: WebServer2;
 
 import mkk_site.view_service.service;
 
+// Подключение разделов сервиса
 import mkk_site.view_service.index;
 import mkk_site.view_service.moder;
 import mkk_site.view_service.auth;
 import mkk_site.view_service.controls_test;
 import mkk_site.view_service.pohod;
+import mkk_site.view_service.document;
+import mkk_site.view_service.tourist_list;
+//import mkk_site.view_service.experience;
 
 void main(string[] progAgs)
 {
