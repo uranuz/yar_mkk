e72ec4769c04fc3c6df4f1b728d11b77d0871af5
uranuz <neuranuz@gmail.com>
2017-12-16 19:58:15 +0300

    Более красивый интерфейсный код в статистике

--- client/mkk/Stat/Stat.js
+++ client/mkk/Stat/Stat.js
@@ -10,210 +10,195 @@ define('mkk/Stat/Stat', [
 
 	function Stat(opts) {
 		FirControl.call(this, opts);
-		this._name = 'Test';
 		this._elems('byYearBtn').on('click', this.reloadPage);
 		this._elems('byComplexityBtn').on('click', this.reloadPage);
-		doFlot();
-		
-	}; 
-
-	var
-		dx = [],
-		a01 = [],
-		a02 = [],
-		a03 = [],
-		a04 = [],
-		a05 = [],
-		a06 = [],
-		a07 = [],
-		a08 = [],
-		a09 = [],
-		a10 = [],
-		a11 = [];
-
-/*
-function reloadPage()
-{
-	document.forms.main_form.submit()
-}
-*/
-
-function doFlot ()
-{ 
-	var f,i,j;
-
-	if( prez == 0 )
-	{
-		for( j = 0; j < y; j += 1 )
-		{
-			f = new Date(2000,12,31);
-			f = Date.parse(Surs0[j]);
-			dx.push(f);
-		}
-		t=y;
-	}
-			
-	if( prez == 1 ) {
-		dx = Surs0;
-		t = 8;
-	}
-
-	for (i = 0; i < t; i+= 1)
-	{
-		if(boolGraf[1]) a01.push([dx [i], Surs1[i]]);
-		if(boolGraf[2]) a02.push([dx [i], Surs2[i]]);
-		if(boolGraf[3]) a03.push([dx [i], Surs3[i]]);
-		if(boolGraf[4]) a04.push([dx [i], Surs4[i]]);
-		if(boolGraf[5]) a05.push([dx [i], Surs5[i]]);
-		if(boolGraf[6]) a06.push([dx [i], Surs6[i]]);
-		if(boolGraf[7]) a07.push([dx [i], Surs7[i]]);
-		if(boolGraf[8]) a08.push([dx [i], Surs8[i]]);
-		if(boolGraf[9]) a09.push([dx [i], Surs9[i]]);
-		if(boolGraf[10]) a10.push([dx [i], Surs10[i]]);
-		if(boolGraf[11]) a11.push([dx [i], Surs11[i]]);
-	} 
-
-	var r1,r2,r3;
-	if( prez == 0 ) {
-		r1 = null;
-		r2 = "time";
-		r3 = 0570000022408;
-	} 
-
-	if( prez == 1 ) {
-		r1 = [
-			[0, "н/к"],
-			[1, "1 кс"],
-			[2, "2 кс"],
-			[3, "3 кс"],
-			[4, "4 кс"],
-			[5, "5 кс"],
-			[6, "6 кс"],
-			[7, "пут."]
-		],
-		r2 = null,
-		r3 = 0
-	}
-
-	var chartConfig = {
-		xaxis: {
-			ticks: r1,
-			mode: r2,
-			min: r3
-		},
-		legend: {
-			show: true,
-			container: $('#legend')
-		},
-		yaxis: {
-			min: 0
-		},
-		x2axis: {},
-		y2axis: {},
-		points: {
-			show: true, //узловые точки
-			radius: 4, //радиус кружка
-			fill: true //заполнить кружок
-			//fillColor: "grid" //цвет заполнения
-		},
-		lines: {
-			show: true,
-			lineWidth: 4,
-			fill: false,
-			fillColor: '#ca0000'
-		},
-		bars: {
-			show: false,
-			horizontal: false, // столбцы горизонтально
-			shadowSize: 0, // тени размер
-			barWidth: 0.5 //ширина столбца
-		},
-		grid: {
-			clickable: true,
-			hoverable: true,
-			autoHighlight: true,
-			mouseActiveRadius: 15
-		},
-		selection: {},
-		shadowSize: 5, //тени
-		colors: [
-			'#DC143C', // все
-			'#228B22', // пеший
-			'#AFEEEE', // лыжный
-			'#F4A460', // горный
-			'#1E90FF', // водный
-			'#696969', // вело
-			'#151B54', // авто
-			'#3EA99F', // спелео
-			'#E0FFFF', // парус
-			'#7E3517', // конный
-			'#9400D3' // комби
-		] // цвета графиков
+		this._opts = opts;
+		this._tooltipWindow = this._elems('plotTooltip');
+		this.doFlot();
 	};
 
-	// и строим саму диагрумму
-	$.plot($("#placeholder"), [
-		{
-			data: a11,
-			label: "Все",
-			lines: {
-				show: true,
-				lineWidth: 8,
-				fill: true,
-				fillColor: 'rgba(107,142,35,0.3)'
-			}
-		},
-		{ data:a01, label:"Пеший" },
-		{ data:a02, label: "Лыжный" },
-		{ data:a03, label: "Горный" },
-		{ data:a04, label: "Водный" },
-		{ data:a05, label: "Вело" },
-		{ data:a06, label: "Авто" },
-		{ data:a07, label: "Спелео" },
-		{ data:a08, label: "Парус" },
-		{ data:a09, label: "Конный" },
-		{ data:a10, label: "Комби" }
-	], chartConfig);
-}
-
-	function showTooltip(x, y, contents) {
-		$('<div id="tooltip">' + contents + '</div>').css({
-			position: 'absolute',
-			display: 'none',
-			top: y + 5, left: x + 5,
-			border: 'solid 2px #598d23',
-			"border-radius":'5px',
-			padding: '2px',
-			'background-color': '#fee',
-			opacity: 0.80
-		}).appendTo("body").fadeIn(200);
-	}
-
-	$("#placeholder").bind("plothover",
-		function (event, pos, item) {
-			$("#tooltip").remove();
-			if( !item ) return;
-			var
-				k_s = ["н/к", "1 кс", "2 кс", "3 кс", "4 кс", "5 кс", "6 кс", "пут."],
-				x = item.datapoint[0].toFixed(2),
-				y = item.datapoint[1].toFixed(2),
-				dates = new Date(Number(x)),
-				label = '';
-				if( prez == 0 )
-					label = item.series.label + "<br/>" +  Math.floor(y) + " чел.<br/> " + dates.getFullYear()+ " г.";
-				if( prez == 1 )
-					label = item.series.label+"<br/>"+  Math.floor(y) + " чел. <br/>" + k_s[Number(x)];
-
-			showTooltip(item.pageX, item.pageY,  label);
-		}
-	);
-
 	return __mixinProto(Stat, {
+		_subscribeInternal: function() {
+			var self = this;
+			this._elems('plot').on('plothover', this._onPlot_over.bind(this));
+			// Сохраняем обработчик привязанный к классу, чтобы корректно отписаться от события
+			this._resizeBeginHandler = this._onWindow_ResizeBegin.bind(this);
+			$(window).on('resize', this._resizeBeginHandler);
+		},
+		_unsubscribeInternal: function() {
+			this._elems('plot').off();
+			$(window).off('resize', this._resizeBeginHandler);
+		},
 		getName: function() {
 			return this._name;
 		},
 		reloadPage: function() {
 			document.forms.main_form.submit()
+		},
+		/** Обработчик добавляет задержку перед запуском операции,
+		 * запускаемой по изменению размеру окна, чтобы уменьшить нагрузку на браузер
+		 */
+		_onWindow_ResizeBegin: function() {
+			clearTimeout(this._resizeTimerId); // Удаляем таймер, запущенный недавно
+			this._resizeTimerId = setTimeout(this._onWindow_Resize.bind(this), 100);
+		},
+		_onWindow_Resize: function() {
+			this.doFlot(); // Перерисовываем график по изменению размера окна
+		},
+		_onPlot_over: function (event, pos, item) {
+			this._tooltipWindow.remove();
+			if( !item ) return;
+			var
+				grafMode = this._opts.select.conduct,
+				k_s = ["н/к", "1 кс", "2 кс", "3 кс", "4 кс", "5 кс", "6 кс", "пут."],
+				x = item.datapoint[0].toFixed(2),
+				y = item.datapoint[1].toFixed(2),
+				dates = new Date(Number(x)),
+				label = '';
+				if( grafMode == 0 )
+					label = item.series.label + "<br/>" +  Math.floor(y) + " чел.<br/> " + dates.getFullYear() + " г.";
+				if( grafMode == 1 )
+					label = item.series.label + "<br/>"+  Math.floor(y) + " чел. <br/>" + k_s[Number(x)];
+
+			this.showTooltip(item.pageX, item.pageY,  label);
+		},
+		showTooltip: function (x, y, contents) {
+			this._tooltipWindow.css({
+				top: y + 5,
+				left: x + 5
+			}).html(contents).appendTo("body").fadeIn(200);
+		},
+		/**
+		 * Подготовка данных для построения графика
+		 */
+		preparePlotData: function() {
+			if( this._dataSeries && this._chartConfig ) {
+				return; // Данные готовы - не пересчитываем их
+			}
+
+			var
+				grafMode = this._opts.select.conduct,
+				boolGraf = this._opts.data.boolGraf,
+				graf = this._opts.data.graf,
+				grafLength = this._opts.data.grafLength,
+				dataSeries = [
+					{ label: "Пеший" },
+					{ label: "Лыжный" },
+					{ label: "Горный" },
+					{ label: "Водный" },
+					{ label: "Вело" },
+					{ label: "Авто" },
+					{ label: "Спелео" },
+					{ label: "Парус" },
+					{ label: "Конный" },
+					{ label: "Комби" },
+					{
+						label: "Все",
+						lines: {
+							show: true,
+							lineWidth: 8,
+							fill: true,
+							fillColor: 'rgba(107, 142, 35, 0.3)'
+						}
+					}
+				],
+				xAxisData = [];
+		
+			if( grafMode == 0 ) {
+				for( var j = 0; j < grafLength; j += 1 ) {
+					xAxisData.push(Date.parse(graf[0][j]));
+				}
+			}
+					
+			if( grafMode == 1 ) {
+				xAxisData = graf[0];
+				grafLength = 8;
+			}
+		
+			for( var i = 0; i < grafLength; ++i ) {
+				// i - номер точки с данными для графика
+				for( var m = 0; m < graf.length; ++m ) {
+					// m - номер графика
+					// 0 позиция в boolGraf и graf занята для оси x
+					if(boolGraf[m+1]) {
+						// В dataSeries ось x не нужна т.к., передается через xAxisData
+						dataSeries[m].data = dataSeries[m].data || [];
+						dataSeries[m].data.push([xAxisData[i], graf[m+1][i]]);
+					}
+				}
+			}
+
+			var chartConfig = {
+				xaxis: {
+					ticks: (grafMode === 0? null: [
+						[0, "н/к"],
+						[1, "1 кс"],
+						[2, "2 кс"],
+						[3, "3 кс"],
+						[4, "4 кс"],
+						[5, "5 кс"],
+						[6, "6 кс"],
+						[7, "пут."]
+					]),
+					mode: (grafMode === 0? "time": null),
+					min: (grafMode === 0? 570000022408: 0)
+				},
+				legend: {
+					show: true,
+					container: this._elems('plotLegend')
+				},
+				yaxis: {
+					min: 0
+				},
+				x2axis: {},
+				y2axis: {},
+				points: {
+					show: true, //узловые точки
+					radius: 4, //радиус кружка
+					fill: true //заполнить кружок
+					//fillColor: "grid" //цвет заполнения
+				},
+				lines: {
+					show: true,
+					lineWidth: 4,
+					fill: false,
+					fillColor: '#ca0000'
+				},
+				bars: {
+					show: false,
+					horizontal: false, // столбцы горизонтально
+					shadowSize: 0, // тени размер
+					barWidth: 0.5 //ширина столбца
+				},
+				grid: {
+					clickable: true,
+					hoverable: true,
+					autoHighlight: true,
+					mouseActiveRadius: 15
+				},
+				selection: {},
+				shadowSize: 5, //тени
+				colors: [
+					'#228B22', // пеший
+					'#AFEEEE', // лыжный
+					'#F4A460', // горный
+					'#1E90FF', // водный
+					'#696969', // вело
+					'#151B54', // авто
+					'#3EA99F', // спелео
+					'#E0FFFF', // парус
+					'#7E3517', // конный
+					'#9400D3', // комби
+					'#DC143C' // все
+				] // цвета графиков
+			};
+			this._dataSeries = dataSeries;
+			this._chartConfig = chartConfig;
+		},
+		doFlot: function () {
+			this.preparePlotData();
+			// и строим саму диагрумму
+			$.plot(this._elems('plot'), this._dataSeries, this._chartConfig);
 		}
 	});
 });
