8f669bd29c91a8d7c6ad67ca7bb7cdb06833e2e8
uranuz <neuranuz@gmail.com>
2021-09-12 17:03:16 +0300

    Migrating to new build system

--- gulpfile.js
+++ gulpfile.js
@@ -2,114 +2,20 @@
 
 var
 	path = require('path'),
-	gulp = require('gulp'),
-	vfs = require('vinyl-fs'),
-	shell = require('gulp-shell'),
-	gulpClean = require('gulp-clean'),
-	gulpCopy = require('gulp-copy'),
-	gulpHelpers = require('./client/gulp_helpers');
+	tfConfig = require('../trifle/builder/config'),
+	tfBuilder = require('../trifle/builder/builder');
 
-var sites = gulpHelpers.resolveConfig();
+function makeConfig() {
+	var config = tfConfig.resolveConfig();
 
+	// This gulp script runs scripts for all sites in repository
+	config.dependGulpFiles = [
+		path.resolve('./client/mkk.gulpfile.js')
+	];
 
-gulp.task("mkk-symlink", function() {
-	return gulp.src([
-			'./client/*'
-		], {
-			base: './client/',
-			ignore: './client/*.gulpfile.js'
-		})
-		.pipe(vfs.symlink(sites.mkk.outBuild));
-});
-
-gulp.task("mkk-copy", function() {
-	return gulp.src([
-			'./client/mkk.gulpfile.js'
-		])
-		.pipe(gulp.dest(sites.mkk.outBuild));
-});
-
-gulp.task("fir-symlink", function() {
-	return gulp.src([
-			'../fir/fir/'
-		], {base: '../fir/'})
-		.pipe(vfs.symlink(sites.mkk.outBuild));
-});
-
-// Для gulpfile приходится создавать копию вместо симлинка - иначе не работает. Видимо, баг в gulp-cli или gulp
-gulp.task("fir-copy", function() {
-	return gulp.src([
-			'../fir/fir.gulpfile.js'
-		])
-		.pipe(gulp.dest(sites.mkk.outBuild));
-});
-
-gulp.task("ivy-symlink", function() {
-	return gulp.src([
-			'../ivy/ivy/'
-		], {base: '../ivy/'})
-		.pipe(vfs.symlink(sites.mkk.outBuild));
-});
-
-gulp.task("ivy-copy", function() {
-	return gulp.src([
-			'../ivy/ivy.gulpfile.js'
-		])
-		.pipe(gulp.dest(sites.mkk.outBuild));
-});
-
-gulp.task("mkk-node-modules-symlink", function() {
-	return gulp.src([
-			'node_modules'
-		], {base: './'})
-		.pipe(vfs.symlink(sites.mkk.outBuild));
-});
-
-gulp.task("mkk-clean-build", function() {
-	return gulp.src([sites.mkk.outBuild], {
-		read: false,
-		allowEmpty: true
-	}).pipe(gulpClean({force: true}));
-});
-
-gulp.task("mkk-symlink-to-build", gulp.parallel([
-	'mkk-symlink',
-	'mkk-copy',
-	'fir-symlink',
-	'fir-copy',
-	'ivy-symlink',
-	'ivy-copy',
-	'mkk-node-modules-symlink'
-]));
-
-function runBuildOpts(site) {
-	var opts = ['gulp'];
-	if( !sites.hasOwnProperty(site) ) {
-		throw new Error('There is no site "' + site + '" in sites configuration!');
-	}
-	opts.push('--gulpfile="' + path.resolve(sites[site].outBuild, site + '.gulpfile.js') + '"');
-	//opts.push('--cwd="' + path.resolve(sites[site].outBuild, site) + '"');
-	var optNames = gulpHelpers.CMD_LINE_OPTS;
-	optNames.forEach(function(it) {
-		if( site[it] ) {
-			opts.push('--' + it + '="' + site[it] + '"');
-		}
-	});
-	return opts.join(' ');
+	return config;
 }
 
-gulp.task("mkk-run-build", shell.task(
-	runBuildOpts('mkk'),
-	{
-		cwd: path.resolve(sites.mkk.outBuild) // Set current working dir
-	}
-));
-
-gulp.task("mkk", gulp.series([
-	"mkk-clean-build",
-	"mkk-symlink-to-build",
-	"mkk-run-build"
-]));
-
-gulp.task("default", gulp.series(['mkk']));
-
+Object.assign(exports, {
+	default: tfBuilder.makeTasks(makeConfig())
+});
